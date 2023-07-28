24307de78a0ab8b99e4a1cd779123c72ed04431d
ailinbersano <bersanoailin@gmail.com>
2023-07-28 10:55:01 -0300

    Se creo la vista de cuentas se agregó el controlador que devuelve los clientes

--- HomeBankingMindHub/Models/DbInitializer.cs
+++ HomeBankingMindHub/Models/DbInitializer.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-
 namespace HomeBankingMindHub.Models
 {
     public class DBInitializer
@@ -11,16 +10,55 @@ namespace HomeBankingMindHub.Models
             {
                 var clients = new Client[]
                 {
-                    new Client { Email = "vcoronado@gmail.com", FirstName="Victor", LastName="Coronado", Password="123456"}
+                    new Client { Email = "vcoronado@gmail.com",
+                        FirstName="Victor",
+                        LastName="Coronado",
+                        Password="123456"
+                    },
+                    new Client { Email = "estebanrey@gmail.com",
+                        FirstName="Esteban",
+                        LastName="Rey",
+                        Password="357384"
+                    },
+                    new Client { Email = "juliarobert@gmail.com",
+                        FirstName="Julia",
+                        LastName="Robert",
+                        Password="10101010"
+                    }
                 };
-
+                
                 foreach (Client client in clients)
                 {
                     context.Clients.Add(client);
                 }
-
                 //guardamos
                 context.SaveChanges();
+            }
+
+            if(!context.Accounts.Any())
+            {
+                var accountVictor = context.Clients.FirstOrDefault(
+                    c => c.Email == "vcoronado@gmail.com");
+                if (accountVictor != null)
+                {
+                    var accounts = new Account[]
+                    {
+                        new Account
+                        {
+                            ClientId=accountVictor.Id,
+                            CreationDate=DateTime.Now,
+                            Number=string.Empty,
+                            Balance=0
+                        }
+                    };
+                    foreach (Account account in accounts)
+                    {
+                        context.Accounts.Add(account);
+                    }
+
+                    //guardamos
+                    context.SaveChanges();
+                }
             }
 
         }
--- HomeBankingMindHub/Models/HomeBankingContext.cs
+++ HomeBankingMindHub/Models/HomeBankingContext.cs
@@ -4,13 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using System.Threading.Tasks;
+
 namespace HomeBankingMindHub.Models
 {
     public class HomeBankingContext : DbContext
     {
         public HomeBankingContext(DbContextOptions<HomeBankingContext> options) : base(options) { }
-
+        
         //dbsets
         public DbSet<Client> Clients { get; set; }
+        public DbSet<Account> Accounts { get; set; }
     }
 }
