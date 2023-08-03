303327c60521726bb1fcfd289fed71ac4e3944ec
ailinbersano <bersanoailin@gmail.com>
2023-08-03 12:27:36 -0300

    rdtfygh

--- HomeBankingMindHub/Repositories/ClientRepository.cs
+++ HomeBankingMindHub/Repositories/ClientRepository.cs
@@ -18,6 +18,15 @@ namespace HomeBankingMindHub.Repositories
                 .Include(client => client.Cards)
                 .FirstOrDefault();
         }
+        public Client FindByEmail(string email)
+        {
+            return FindByCondition(client => client.Email.ToUpper() == email.ToUpper())
+            .Include(client => client.Accounts)
+            .Include(client => client.ClientLoans)
+                .ThenInclude(cl => cl.Loan)
+            .Include(client => client.Cards)
+            .FirstOrDefault();
+        }
         public IEnumerable<Client> GetAllClients()
         {
             return FindAll()
--- HomeBankingMindHub/Startup.cs
+++ HomeBankingMindHub/Startup.cs
@@ -1,7 +1,9 @@
 using HomeBankingMindHub.Models;
 using HomeBankingMindHub.Repositories;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,6 +34,19 @@ namespace HomeBankingMindHub
             services.AddDbContext<HomeBankingContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("HomeBankingConexion")));
             services.AddScoped<IClientRepository,ClientRepository>();
             services.AddScoped<IAccountRepository, AccountRepository>();
+            //autenticación
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options =>
+            {
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(10);
+                options.LoginPath = new PathString("/index.html");
+            });
+
+            //autorización
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("ClientOnly", policy => policy.RequireClaim("Client"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,12 +64,17 @@ namespace HomeBankingMindHub
             app.UseStaticFiles();
 
             app.UseRouting();
+            //le decimos que use autenticación
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages(); 
+                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=games}/{ action = Get}");
                 endpoints.MapControllers();
             });
         }
